86f352189bb12fe6871666241d9b23ec572df996
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-19 16:47:59 -0400

    finished implementation for selection

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,17 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int lastInd = 0;
+        int index = 0;
+        for(String song : playList){
+            if(song.equals(selection)){
+                lastInd = index;
+            }
+            index++;
+        }
+        int result = Math.abs(startIndex-lastInd) < playList.length/2 ?
+                Math.abs(startIndex-lastInd) : playList.length - Math.abs(startIndex-lastInd);
+        return result;
     }
+
 }
